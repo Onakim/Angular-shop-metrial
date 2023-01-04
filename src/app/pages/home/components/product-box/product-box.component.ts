ef4afad1b97713daba0472353b95fbb410ad0b6a
Andrey Mekeda <andreymekeda@gmail.com>
2023-01-04 12:06:34 +0200

    add product and edit grid

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-product-box",
   templateUrl: "./product-box.component.html",
   styles: [],
 })
-export class ProductBoxComponent {}
+export class ProductBoxComponent {
+  @Input() fullWhidthMode = false;
+}
