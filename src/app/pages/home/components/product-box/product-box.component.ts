82280371dcd9ff5d3b821623557e4396da895de7
Andrey Mekeda <andreymekeda@gmail.com>
2023-01-04 11:46:28 +0200

    add product box

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-product-box",
+  templateUrl: "./product-box.component.html",
+  styles: [],
+})
+export class ProductBoxComponent {}
