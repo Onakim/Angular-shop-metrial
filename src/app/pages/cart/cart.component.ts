02b2eaea4138b65a0736a1eb12aed7556e453270
Andrey Mekeda <andreymekeda@gmail.com>
2023-02-02 21:57:44 +0200

    add server and stripe service for payment

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { loadStripe } from "@stripe/stripe-js";
 import { Cart, CartItem } from "src/app/models/cart.model";
 import { CartService } from "src/app/services/cart.service";
 
@@ -35,7 +37,7 @@ export class CartComponent {
     "total",
     "action",
   ];
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient) {}
   ngOnInit(): void {
     this.cartService.cart.subscribe((_cart: Cart) => {
       this.cart = _cart;
@@ -56,5 +58,18 @@ export class CartComponent {
   }
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http
+      .post("http://localhost:4242/checkout", {
+        items: this.cart.items,
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe("pk_test_7wkTzBd6wHidVvpfusNjGI9t");
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
   }
 }
